666fb3f4ac7d520a663f6671dc126d3202097e7c
mimran <mimran@bd.imshealth.com>
2017-11-14 18:23:46 +0600

    Modified display name of ListOfAssignProjectViewModel

--- TeamCatApp/ViewModel/ListOfAssignedProjectViewModel.cs
+++ TeamCatApp/ViewModel/ListOfAssignedProjectViewModel.cs
@@ -1,14 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace TeamCatApp.ViewModel {
     public class ListOfAssignedProjectViewModel {
+        [Display(Name = "Employee")]
         public string EmployeeName { get; set; }
+
+        [Display(Name = "Project")]
         public string ProjectName { get; set; }
+
+        [Display(Name = "Frequency")]
         public string ProjectFrequency { get; set; }
+
+        [Display(Name = "Hours")]
         public string AssignedHour { get; set; }
+
+        [Display(Name = "Date")]
         public string AssignedDate { get; set; }
     }
 }
